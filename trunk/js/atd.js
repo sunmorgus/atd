9dc24ee786de6932a4ef645ebc138b9e9ed1c02b
sunmorgus <sunmorgus@a5a0feca-c7d2-4240-ba65-1df1fb4ca91a>
2013-01-22 14:22:01 +0000

    added htacces; removed jqm buttons
    git-svn-id: file:///media/house/svn/acquirethedoctor@12 a5a0feca-c7d2-4240-ba65-1df1fb4ca91a

--- trunk/js/atd.js
+++ trunk/js/atd.js
@@ -5,19 +5,11 @@ ATD.currentGame = null;
 //Page Events
 //
 $(document).ready(function(){
-	ATD.currentGame = new Game();
-	console.log(ATD.currentGame);
+	var game = new Game();
+    game.SetupCanvas();
+    
+    ATD.currentGame = game;
 });
 //
 //End Page Events
 //
-
-//
-//Button Events
-//
-$('#newGame').live('tap', function(e){
-	var game = ATD.currentGame;
-	if(game !== null){
-		game.SetupCanvas();
-	}
-});
